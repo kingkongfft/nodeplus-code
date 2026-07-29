73c42cd697fe5b66d02b79734ee582fe99eebf98
Your Name <you@example.com>
2026-07-29 18:54:54 +0800

    Release NodePlus-CODE v0.2.0

--- PowerEditor/src/resource.h
+++ PowerEditor/src/resource.h
@@ -19,7 +19,7 @@
 
 //************ Notepad++ version **************************
 
-#define NOTEPAD_PLUS_VERSION L"Notepad++ v8.9.7"
+#define NOTEPAD_PLUS_VERSION L"NodePlus-CODE v0.2.0 (N++ 8.9.7)"
 
 // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71
 // ex : #define VERSION_VALUE L"5.63\0"
